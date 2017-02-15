b5cc33811d8d0d6f0d7279d659f26ca05e0483d8
Jay Connelly <jayhconnelly@gmail.com>
2017-02-15 14:55:44 -0600

    got maxrect2 working

--- MaxRect2/src/maxrect2/MaxRect2.java
+++ MaxRect2/src/maxrect2/MaxRect2.java
@@ -16,20 +16,38 @@ public class MaxRect2 {
     /**
      * @param args the command line arguments
      */
-    static int max = -1;
-    static int num = 0;
-    static ArrayList<Integer> running = new ArrayList<>(); // list of all active sizes
-    static ArrayList<Integer> values = new ArrayList<>(); // list of all active sizes
+    static protected class BldData {
+        private int ref;
+        private int span;
+        public BldData(int r, int s) {
+            ref = r;
+            span = s;
+        }
+        public int getRef() { return ref; }
+        public int getSpan() { return span; }
+        public void incRef(int i) { ref = ref + i; span += i; }
+        public void incSpan(int i) { span = span + i; }
+
+        @Override
+        public String toString() {
+            return("[" + ref + "," + span + "]");
+        }
+        
+    }
+    static int max = -1;  // max value so far - must be >= 0
+    static int num = 0;  // number of buildings 
     static ArrayList<Integer> a = new ArrayList<>();   // array of all the buildings
+    static Map<Integer,BldData> m = new LinkedHashMap();  // hashmap of all active sizes
+    
     
     static void getData(ArrayList<Integer> a) {
-        Scanner sc = new Scanner("3 4 3 3");
-        num = sc.nextInt();
-        //System.out.println("num = " + num);
+        Scanner sc = new Scanner("2 2 5 6 4 5 1 3 2 1");
+        num = 0;
         int next;
-        for (int i = 0; i < num; i++) {
+        while (sc.hasNext()) {
+            num++;
             next = sc.nextInt();
-            a.add(i,next);
+            a.add(next);
         }
         //System.out.println(a);
     }
@@ -37,23 +55,102 @@ public class MaxRect2 {
     public static void main(String[] args) {
         // TODO code application logic here
         Integer b;
-        int tmp = 0;
+        Integer tmp = 0;
         getData(a);
-        Set<Integer> s = new HashSet();  // set of running floor sizes
+        Stack stk = new Stack();
+
         for (int i = 0; i < num; i++) { // now loop through the buildings
             b = a.get(i); // get the next building height
-            System.out.println(" got height of " + b);
-            
-            if (running.contains(b)) {
-                values.set(i, values.get(i)+1);  // increment the run
-                System.out.println(" setting element " + b + " to " + values.get(i));
-            } else {
-                System.out.println(" adding element " + b);
-                values.add(i,1);
-                running.add(i,b);
+
+           BldData bldData;     
+            // see if this is new
+            if (m.isEmpty() || ! m.containsKey(b)) { // then we just add it
+                bldData = new BldData(1,1);
+                m.put(b,bldData);
+                //System.out.println(" " + m.get(b).getRef()); 
+            } else {  // found a duplicate  
+                //System.out.print(" incrementing ref count for " + b + " to " );
+                bldData =  (BldData) m.get(b);
+                bldData.incRef(1);
+                //System.out.println( m.get(b).getRef());
             }
-            System.out.println(" running " + running.toString() + "values " + values.toString());
-        }  
+            //System.out.println(" Map now looks like: " + m.toString());
+           
+            // now loop through the active set to figure out what to inc and what to retire
+            Set set = m.entrySet();
+            Iterator itr =  set.iterator();
+            while (itr.hasNext()) {
+                Map.Entry next = (Map.Entry)itr.next();
+                // System.out.println(" Processing " + next.toString() + " Map = " + m.toString());
+                if ( (Integer) next.getKey() < b) {  // if key is less than what we are adding, increment span
+                    bldData =  (BldData) m.get( (Integer) next.getKey());
+                    bldData.incSpan(1);
+                    //System.out.println("New span count for " + next.getKey() + " = " +  m.get( (Integer) next.getKey()).getSpan());
+                    
+                } else if ( (Integer) next.getKey() > b) {
+                    // retire next
+                    Integer key = (Integer) next.getKey();
+                    bldData = (BldData) next.getValue();
+                    if ((key * bldData.getSpan()) > max) max = key* bldData.getSpan();
+                    //System.out.print(" retiring " + next.toString() + " Max = " + max);
+                    stk.push(key);
+                          
+                   // System.out.println(" pushed  " + key + " map = " + m.toString()) ;
+
+                }
+            } // end while
+            
  
-    }
+            // now clean up and deal with deletes
+            int d = 0; // delete item
+            int minValue = Integer.MAX_VALUE;
+            int tt=0;
+            int size = stk.size();
+            
+            //System.out.println(" Stack = " + stk.toString() + "size = " + stk.size());
+            
+            // now deal with the ones we retired
+            for (int t=0; t< size; t++) {
+                
+                d = (Integer) stk.pop();
+                //System.out.println(" popped " + d + " map = " + m.toString() );
+                if (d < minValue) {
+                    tt =  m.get(d).getSpan(); // get the number of entries for the minimum
+                    //System.out.println(" found new minimum value of " + d + " with span of " + tt);
+                    minValue = d;
+                }
+                //System.out.println(" Deleting " + d);
+                BldData remove = m.remove( d);
+                if (remove == null) {
+                    System.out.println(" Remove of " + d + " == null");
+                }
+            }
+          
+            if (tt > 0) {
+                BldData bData = m.get(b);
+                if ((bData != null) && (bData.getRef() <= 1)) {
+                    bData.incSpan(tt);
+                    m.put( b, bData);
+                    //System.out.println(" bumped " + b + " to " + m.get(b) + " Map = " + m.toString() );
+                } 
+            }
+            //System.out.println(" Max == " + max);
+         
+        }
+   
+        
+        // now deal with the still active ones
+        Set set = m.entrySet();        
+        Iterator itr =  set.iterator();
+        while (itr.hasNext()) {
+            Map.Entry me = (Map.Entry) itr.next();
+            Integer key = (Integer) me.getKey();
+            Integer value = ((BldData) me.getValue()).getSpan();
+            Integer tmp1 = key *  value;
+            if (tmp1 > max) max = tmp1;
+           // System.out.println(" closing out " + key + " value = " + value + " max = " + max);
+            
+        }
+        System.out.println(max);
+    }        
 }
