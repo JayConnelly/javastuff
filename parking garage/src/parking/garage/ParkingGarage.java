e695ad434f0567d40c6a8a60dcf93f2b25c23718
Jay Connelly <jayhconnelly@gmail.com>
2017-03-07 10:35:37 -0600

    got blocked motorcycle spots figured out

--- parking garage/src/parking/garage/ParkingGarage.java
+++ parking garage/src/parking/garage/ParkingGarage.java
@@ -13,15 +13,89 @@ import java.io.*;
  * @author Jay
  */
 public class ParkingGarage {
-    static final int CAR = 0;
-    static final int MOTORCYCLE = 1;
-    static int numCarSlots = -1;
-    static int numMotorSlots = -1;    
-    static int numCarSpots = -1;
-    static int numMotorSpots = -1;
-    static int type = -1;
-    static boolean allowed;
+    static final String CAR = "C";          // input format for car 
+    static final String  MOTORCYCLE = "M";  // input format for motorcycle
     
+    static final int CARSIZE = 3;           // width of car 
+    static final int  MOTORCYCLESIZE = 1;   // width of motorcycle
+
+    static int numCarSpots = -1;        // number of spots designated for cars
+    static int numMotorSpots = -1;      // number of spots designated for motor cycles
+    static int numBlocked = 0;        // worst mumber of motorCycle Spots blocked
+    
+    // next couple are only for parking in motorcycle slots. probably better names to be had
+    static boolean isSpaceForCar() { return ((numMotorSpots - numBlocked) >= CARSIZE); }
+    static boolean isSpaceForMotorCycle() { return (numMotorSpots  >= MOTORCYCLESIZE); }
+    
+    protected static class Vehicle {
+        static int numVehicles = 0;
+        private int size = 0;       // 1 for notorcycle, 3 for car
+        private String name;        // printable name based on type
+        String parkedAt;    // so we can unwind when it leaves
+
+        void dump() { 
+            if (parkedAt != null) {
+                System.out.printf( "Vehicle %d is a %s parked in %s \n ", numVehicles, name, parkedAt);
+            } else {
+                System.out.printf( "Vehicle %d is a %s and is not parked\n ", numVehicles, name);
+
+            }
+         };
+        
+        String getName() { return(name); }
+        
+        boolean isCar() { return( (size == CARSIZE));}
+        boolean isMotorCycle() { return( (size == MOTORCYCLESIZE));}
+        
+        
+        boolean tryToPark(String typeOfSpot) {           // park the vehicle and adjust totals
+            boolean allowed = false;
+            if (typeOfSpot.equals(MOTORCYCLE)) {        // parking in a motorcycle spot
+                //System.out.println(" Trying to parking in motorcycle space");
+                if ( (isCar() && isSpaceForCar()) || (isMotorCycle() && isSpaceForMotorCycle()) )   {  
+                    //System.out.println(" parking in motorcycle space");
+                    numMotorSpots  -= size;      // remove actual spots used 
+                    numBlocked += 2;    // blocks 2 additional spaces for cars
+                    parkedAt = MOTORCYCLE;      // remember where we parked for when we leave
+                    allowed = true;
+                } 
+            } else if (typeOfSpot.equals(CAR) && (numCarSpots > 0) ) {                         // parking in  car spot
+                    //System.out.println(" parking in car space");
+                    numCarSpots -= 1;        // must have taken a car slot
+                    parkedAt = CAR;         // remember where we parked for when we leave
+                    allowed = true;
+            }
+            
+            //System.out.printf("parking in %s = %b ,  %b %b %b %b %d \n",typeOfSpot, allowed, isCar(),isSpaceForCar(), isMotorCycle(), isSpaceForMotorCycle(), numBlocked);
+            //System.out.printf("motorspots =  %d,  blocked = %d \n" , numMotorSpots, numBlocked);
+            //dump();
+            return(allowed);
+        }
+                
+        boolean canPark() {
+            if (isCar()) {      // cars park in motorcycle first or car slot 2nd
+                return (tryToPark(MOTORCYCLE) || tryToPark(CAR));
+            } else {            // motorcycles park in car spots first then motorcycles if avauilable
+                return (tryToPark(CAR) || tryToPark(MOTORCYCLE));
+            }
+        }
+                    
+        Vehicle( String vString) {
+            switch (vString) {
+                case CAR:
+                    size = 3;
+                    name = CAR;
+                    break;
+                case MOTORCYCLE:
+                    size = 1;
+                    name = MOTORCYCLE;
+                    break;
+                default:
+                    System.out.println(" Invalid vehicle type " + vString);
+            }
+            numVehicles++;
+        }
+    }
      
     public static void main(String[] args) throws FileNotFoundException {
         
@@ -30,34 +104,14 @@ public class ParkingGarage {
         
         numCarSpots = sc.nextInt();
         numMotorSpots = sc.nextInt();
-        allowed = true;
-        
-        while ( sc.hasNext() && ((numMotorSpots > 0) || (numCarSpots > 0) )) {
-            allowed = true;   // set default then unwind if not possible
-            type = sc.nextInt();
-          
-            if (type == CAR) {   // its a car
-                if (numMotorSpots  > 3) {
-                    numMotorSpots  -= 3;  // worse case - assume they parked in the motorcycle spots
-                } else if (numCarSpots > 0) {
-                    numCarSpots -= 1;        // must have taken a car slot
-                } else {                  // not enough car slots
-                    allowed = false;
-                } 
-            } else if (type == MOTORCYCLE )  {
-                if (numCarSpots >= 1) {
-                    numCarSpots -= 1;  // worse case he parks in a car spot
-                } else if (numMotorSpots > 0) {
-                    numMotorSpots -= 1;   // has to take a motorcycle slot
-                } else {
-                    allowed = false;
-                }
-            }
+                
+        while ( sc.hasNext() ) {   // keep going while vehicles are coming
+            Vehicle v  = new Vehicle((String) sc.next());
     
-            if (allowed) {
-                System.out.println("Welcome " + type);
+            if (v.canPark()) {
+                System.out.println("Welcome " + v.getName());
             } else {
-                System.out.println("Sorry - all full " + type);
+                System.out.println("Sorry - all full " + v.getName());
             }
         }
     }
