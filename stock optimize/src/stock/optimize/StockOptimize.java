69eaadba0898bd6386730c5720689d0a79b26c49
Jay Connelly <jayhconnelly@gmail.com>
2017-06-16 15:11:41 -0500

    added html5 application

---
+++ stock optimize/src/stock/optimize/StockOptimize.java
@@ -0,0 +1,32 @@
+/*
+ Your algorithms have become so good at predicting the market that you now know what the share price of Wooden Orange Toothpicks Inc. (WOT) will be for the next N days.
+
+Each day, you can either buy one share of WOT, sell any number of shares of WOT that you own, or not make any transaction at all. What is the maximum profit you can obtain with an optimum trading strategy?
+
+Input Format
+
+The first line contains the number of test cases T. T test cases follow:
+
+The first line of each test case contains a number N. The next line contains N integers, denoting the predicted price of WOT shares for the next N days.
+
+Constraints
+
+All share prices are between 1 and 100000
+
+ */
+package stock.optimize;
+
+/**
+ *
+ * @author Jay
+ */
+public class StockOptimize {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
